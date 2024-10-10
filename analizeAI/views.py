9225c3fff767878ef5919e7e7b707c0fb2b73ae5
rogelioolarte <rogelioolarte2222@gmail.com>
2024-10-10 14:30:17 -0500

    added endpoint changes

--- analizeAI/views.py
+++ analizeAI/views.py
@@ -4,7 +4,7 @@ import PyPDF2
 from datetime import datetime
 
 from django.http import HttpResponse
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 import google.generativeai as genai
 
@@ -22,35 +22,50 @@ def index(request):
 
 @csrf_exempt
 def post(request):
-    # Aquí puedes procesar el archivo PDF y la descripción del trabajo
+    try:
+        if request.method != 'POST':
+            return HttpResponseBadRequest("Method not allowed. Only POST requests are allowed.")
 
-    # Obtener el archivo PDF y la descripción de la solicitud
-    pdf_file = request.FILES['resume_pdf']
-    job_description = request.POST.get('job_description')
+        if 'resume_pdf' not in request.FILES or 'job_description' not in request.POST:
+            return JsonResponse({'error': 'Missing PDF file or job description.'}, status=400)
 
-    # Procesar el PDF
-    text_from_pdf = extract_text_from_pdf(pdf_file)
+        pdf_file = request.FILES['resume_pdf']
+        job_description = request.POST.get('job_description')
 
-    # Configurar la API de Google Generative AI
-    # Coloca tu clave de API aquí
-    genai.configure(api_key=os.environ["API_KEY"])
+        
+        text_from_pdf = extract_text_from_pdf(pdf_file)
+        if(text_from_pdf.__len__ > 0):
+            return JsonResponse({'error': 'The PDF file does not have valid text.'}, status=500)
 
-    # Modelo para analizar el CV y la descripción del trabajo
-    model = genai.GenerativeModel(
-        'gemini-1.5-flash',
-        system_instruction="""
-            You are an experienced Resume reviewer.
-            Your task is to analyze the Resume and Job Description.
-            Respond in Spanish.
-        """,
-        generation_config=genai.GenerationConfig(temperature=0.5),
-    )
+        genai.configure(api_key=os.environ.get("API_KEY"))
+        if not os.environ.get("API_KEY"):
+            return JsonResponse({'error': 'API key not found.'}, status=500)
 
-    # Generar la respuesta basada en el CV y la descripción del trabajo
-    response = model.generate_content(f"My Resume: {text_from_pdf}\nJob Description: {job_description}")
+        model = genai.GenerativeModel(
+            'gemini-1.5-flash',
+            system_instruction="""
+                You are an experienced Resume reviewer.
+                Your task is to analyze the Resume and Job Description.
+                Respond in Spanish.
+            """,
+            generation_config=genai.GenerationConfig(temperature=0.5),
+        )
 
-    # Enviar el texto generado como respuesta en vez de guardarlo en un archivo
-    return JsonResponse({ 'response_text': response.text })
+        response = model.generate_content(f"My Resume: {text_from_pdf}\nJob Description: {job_description}")
+
+        return JsonResponse({'response_text': response.text})
+
+    except KeyError as e:
+        return JsonResponse({'error': f'Missing key in request: {str(e)}'}, status=400)
+
+    except PyPDF2.errors.PdfReadError as e:
+        return JsonResponse({'error': f'Error processing PDF file: {str(e)}'}, status=400)
+
+    except genai.exceptions.APIError as e:
+        return JsonResponse({'error': f'Error in Google Generative AI API: {str(e)}'}, status=500)
+
+    except Exception as e:
+        return JsonResponse({'error': f'An unexpected error occurred: {str(e)}'}, status=500)
 
 def extract_text_from_pdf(pdf_file):
     """Función para extraer texto de un archivo PDF"""

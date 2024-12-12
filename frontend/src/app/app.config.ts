38fdf96fb0161516b0d12201e6f5efd8330adbc4
rogelioolarte <rogelioolarte2222@gmail.com>
2024-12-12 05:21:37 -0500

    Updated style.scss

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -11,7 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(),
-    provideHttpClientTesting()
+    provideHttpClient()
   ]
 };
